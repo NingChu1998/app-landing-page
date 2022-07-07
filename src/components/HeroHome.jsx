bf0726a1fab74b051cd6454e25c4439f4704ab78
Ning Chu <victoryning0309@gmail.com>
2022-07-07 17:52:22 +0800

    useing cloudinary to save picture to reduce the speed of sites

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -8,6 +8,7 @@ import { Carousel } from 'react-responsive-carousel';
 import HerophoneImage1 from '../images/hero/herophone_1.webp';
 import HerophoneImage2 from '../images/hero/herophone_2.webp';
 import HerophoneImage3 from '../images/hero/herophone_3.webp';
+
 import "aos/dist/aos.css"
 
 
@@ -16,7 +17,7 @@ function HeroHome() {
     Aos.init({ duration: 10000});
   }, []);
   return (
-    <div className= "bg-cover ml-auto  mx-auto w-full" style={{backgroundImage: `url(${BackgroundImage})` }}>
+    <div className= "bg-cover ml-auto  mx-auto w-full" style={{backgroundImage: `url(https://res.cloudinary.com/dng8o0y7y/image/upload/v1657186419/Landing-page/bg-png_zbf6c0.webp)` }}>
       <div className="grid align-middle md:grid-cols-2 max-w-6xl mx-auto my-10 pb-0 md:my-10 md:pb-0 ">
         <div data-aos="fade-right" data-aos-delay="300"  className="mt-15 flex flex-col justify-center items-center md:items-start w-full px-2 py-8 md: mt-0">
           <p className="hidden md:grid text-center text-gray-100 text-sm center px-5 ">Transform Everyday</p>
@@ -27,18 +28,18 @@ function HeroHome() {
         <div className='flex flex-cols-2'>
           <div data-aos="fade-left" data-aos-delay="300" className="flex-auto mx-auto ml-5 w-2/5 md:pt-5 md:w-32 md:object-bottom ">
             <div className='w-full h-full pt-5' >
-              <div className=" relative bg-cover ml-5 p-2 md:p-2" style={{backgroundImage: `url(${Mockup})` }}>
+              <div className=" relative bg-cover ml-5 p-2 md:p-2" style={{backgroundImage: `url(https://res.cloudinary.com/dng8o0y7y/image/upload/v1657186419/Landing-page/iPhone-12_lyyvl2.webp)` }}>
                   <Carousel autoPlay infiniteLoop showStatus={false} showThumbs={false} showIndicators={false} showArrows={false} interval={2000}>
-                    <img className="" src={HerophoneImage1} />
-                    <img className=""src={HerophoneImage2} />
-                    <img className="" src={HerophoneImage3} />
+                    <img className="" src="https://res.cloudinary.com/dng8o0y7y/image/upload/v1657186419/Landing-page/herophone_1_hwz3iz.webp" />
+                    <img className=""src="https://res.cloudinary.com/dng8o0y7y/image/upload/v1657186419/Landing-page/herophone_2_qspslp.webp" />
+                    <img className="" src="https://res.cloudinary.com/dng8o0y7y/image/upload/v1657186419/Landing-page/herophone_3_kp05ti.webp" />
                   </Carousel>
               </div>
             </div>
             {/* <img  src={PhoneImage} alt=""  /> */}
           </div>
           <div data-aos="fade-left" data-aos-delay="300" className="flex-auto pb-0 w-3/5 md:w-64 md:object-bottom">
-            <img className='md:object-bottom' src={HeroImage} alt="/" />
+            <img className='md:object-bottom' src="https://res.cloudinary.com/dng8o0y7y/image/upload/v1657186419/Landing-page/hero_figure_nqdg25.webp" alt="/" />
             {/* <changeImage /> */}
           </div>  
         </div>
