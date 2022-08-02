d3eba0a832418114fb3513567f66c159fa95781d
NingChu1998 <68261733+NingChu1998@users.noreply.github.com>
2022-08-02 17:18:05 +0800

    Update JoinWaitlistThankPage.jsx

--- src/pages/JoinWaitlistThankPage.jsx
+++ src/pages/JoinWaitlistThankPage.jsx
@@ -1,5 +1,5 @@
 import Footer from '../components/Footer';
-import { Link } from 'react-router-dom';
+
 
 export default function joinwaitlistthankpage() {
     return (
@@ -13,7 +13,6 @@ export default function joinwaitlistthankpage() {
             <div className='bottom-0'>
                 <Footer />
             </div>
-            <Link to='../joinwaitlist-thankpage'></Link>
         </div>
     )
 }
