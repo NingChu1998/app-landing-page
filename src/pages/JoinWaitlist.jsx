951dbf786b97838341e3ab9be3916661387841fc
Ning Chu <victoryning0309@gmail.com>
2022-08-02 12:10:13 +0800

    update joinwaitlist and suscription

---
+++ src/pages/JoinWaitlist.jsx
@@ -0,0 +1,7 @@
+export default function joinwaitlist() {
+    return (
+      <div className="bg-gray-50 m-0 overflow-hidden w-full h-screen ">
+         <iframe src="https://tally.so/r/w7RoMa?transparentBackground=1" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="Enter your details to get on the waitlist for SuperWOD access"></iframe>
+      </div>
+    )
+  }
---
+++ src/pages/Suscription.jsx
@@ -0,0 +1,8 @@
+a
+export default function suscription() {
+  return (
+    <div className="bg-gray-50 m-0 overflow-hidden w-full h-screen ">
+      <iframe src="https://tally.so/r/wA7xQe?transparentBackground=1" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="Payment Detail "></iframe>
+    </div>
+  )
+}
