f2d94f330eca30218e18d0f0b5cca2e7673a4f38
Ning Chu <victoryning0309@gmail.com>
2022-08-03 12:41:48 +0800

    modify thankyou page

--- src/pages/SuscriptionThankPage.jsx
+++ src/pages/SuscriptionThankPage.jsx
@@ -2,15 +2,15 @@ import Footer from '../components/Footer';
 
 export default function suscriptionthankpage() {
     return (
-        <div className="max-w-7xl mx-auto m-0 overflow-hidden w-full  px-4 sm:px-6 lg:px-8">
-            <div className="max-w-3xl mx-auto my-10 pt-20 md:py-1">
+        <div className="max-w-7xl mx-auto m-0 overflow-hidden w-full h-screen px-4 sm:px-6 lg:px-8">
+            <div className="max-w-3xl mx-auto my-10 pt-20 md:py-10">
                 <h1 className='text-2xl md-text-7xl font-bold px-5 mb-3 md:text-3xl text-center  md:text-center'>Thank you for buying SuperWOD!</h1>
                 <p className='text-1xl px-5 text-center my-5 md:text-center '>We appreciate your business and will be thrilled to send you the invite link as soon as possible. An email with tracking information will be sent to you.</p>
                 <p className='text-1xl px-5 text-center my-5 md:text-center '>Thanks again, and we look forward to seeing you soon.</p>
                 <p className='text-1xl px-5 text-center md:text-center '>Team SuperWOD</p>
                 <img className='w-25 m-0'  src='https://res.cloudinary.com/dng8o0y7y/image/upload/v1659424223/Landing-page/Thankyou_pay_ldoezs.png' alt="" />
-                <Footer />
             </div>
+            <Footer />
         </div>
     )
 }
